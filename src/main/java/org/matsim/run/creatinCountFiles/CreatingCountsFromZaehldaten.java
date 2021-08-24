d4b66e9483ecd93f6ea8606965bc67370187a0be
Marc <dziakowski@vsp.tu-berlin.de>
2021-08-24 12:03:56 +0200

    update

--- src/main/java/org/matsim/run/creatinCountFiles/CreatingCountsFromZaehldaten.java
+++ src/main/java/org/matsim/run/creatinCountFiles/CreatingCountsFromZaehldaten.java
@@ -1,6 +1,9 @@
 package org.matsim.run.creatinCountFiles;
 
-import org.apache.log4j.Logger;
+import org.apache.commons.csv.CSVFormat;
+import org.apache.commons.csv.CSVPrinter;
+import org.apache.logging.log4j.LogManager;
+import org.apache.logging.log4j.Logger;
 import org.apache.poi.ss.usermodel.Sheet;
 import org.apache.poi.xssf.usermodel.XSSFWorkbook;
 import org.matsim.api.core.v01.Coord;
@@ -30,6 +33,7 @@ import org.matsim.counts.Volume;
 import picocli.CommandLine;
 
 import java.io.BufferedWriter;
+import java.nio.file.Path;
 import java.util.ArrayList;
 import java.util.HashMap;
 import java.util.List;
@@ -46,41 +50,39 @@ import static java.util.Objects.requireNonNull;
 
 public class CreatingCountsFromZaehldaten implements MATSimAppCommand {
 
-    private static final Logger logger = Logger.getLogger(CreatingCountsFromZaehldaten.class);
+    private static final Logger logger = LogManager.getLogger(CreatingCountsFromZaehldaten.class);
+
+    private static final CoordinateTransformation CT = TransformationFactory.getCoordinateTransformation("EPSG:31468", "EPSG:25832");
 
     @CommandLine.Option(names = {"-e", "--excel"}, description = "Input count excel file", required = true)
-    private String excel;
+    private Path excel;
 
     @CommandLine.Option(names = {"-n", "--network"}, description = "Input network file", required = true)
-    private String network;
+    private Path network;
 
     @CommandLine.Option(names = {"-o", "--output"}, description = "Output count file", required = true)
-    private String count;
+    private Path count;
 
     public static void main(String[] args) {
-//        new CreatingCountsFromZaehldaten().execute(args);
-        CreatingCountsFromZaehldaten creatingCountsFromZaehldaten = new CreatingCountsFromZaehldaten();
-        creatingCountsFromZaehldaten.call();
+        new CreatingCountsFromZaehldaten().execute(args);
+//        CreatingCountsFromZaehldaten creatingCountsFromZaehldaten = new CreatingCountsFromZaehldaten();
+//        creatingCountsFromZaehldaten.call();
     }
 
     @Override
     public Integer call() {
 
-        excel = "D:/Code/shared-svn/projects/NaMAV/data/Zaehldaten/Zaehldaten.xlsx";
-        network = "Input/leipzig-v1.0-network.xml.gz";
-        count = "";
-
-        Counts<Link> countsPkw = new Counts();
+        Counts<Link> countsPkw = new Counts<>();
         countsPkw.setYear(2018);
         countsPkw.setDescription("data from leipzig to matsim counts");
-        Counts<Link> countsLkw = new Counts();
+        Counts<Link> countsLkw = new Counts<>();
         countsLkw.setYear(2018);
         countsLkw.setDescription("data from leipzig to matsim counts");
 
-        List<leipzigCounts> leipzigCountsList = new ArrayList();
+        List<LeipzigCounts> leipzigCountsList = new ArrayList<>();
 
         try {
-            XSSFWorkbook wb = new XSSFWorkbook(excel);
+            XSSFWorkbook wb = new XSSFWorkbook(excel.toFile());
             Sheet sheet = wb.getSheetAt(0);
             handleSheet(sheet, leipzigCountsList);
         } catch (Exception e) {
@@ -91,10 +93,14 @@ public class CreatingCountsFromZaehldaten implements MATSimAppCommand {
         return null;
     }
 
-    public static void handleSheet(Sheet sheet, List<leipzigCounts> list) {
+    /**
+     * @param sheet
+     * @param list
+     */
+    private static void handleSheet(Sheet sheet, List<LeipzigCounts> list) {
         for (int i = 1; i <= sheet.getLastRowNum(); i++) {
             try {
-                leipzigCounts leipzigCounts = new leipzigCounts();
+                LeipzigCounts leipzigCounts = new LeipzigCounts();
                 leipzigCounts.setStreckenNr((int) sheet.getRow(i).getCell(0).getNumericCellValue());
                 leipzigCounts.setStartNodeID((int) sheet.getRow(i).getCell(1).getNumericCellValue());
                 leipzigCounts.setEndNodeID((int) sheet.getRow(i).getCell(2).getNumericCellValue());
@@ -110,31 +116,30 @@ public class CreatingCountsFromZaehldaten implements MATSimAppCommand {
                 leipzigCounts.setStartNodeCoordY(sheet.getRow(i).getCell(11).getNumericCellValue());
                 leipzigCounts.setEndNodeCoordX(sheet.getRow(i).getCell(12).getNumericCellValue());
                 leipzigCounts.setEndNodeCoordY(sheet.getRow(i).getCell(13).getNumericCellValue());
-                if (leipzigCounts.getYear() > 2017) {
+                if (leipzigCounts.year > 2017) {
                     list.add(leipzigCounts);
                 }
             } catch (Exception e) {
-                e.printStackTrace();
+                logger.error("Error when reading excel data", e);
             }
         }
     }
 
-    private void creatingCounts(List<leipzigCounts> leipzigCountsList, Counts<Link> countsPkw, Counts<Link> countsLkw) {
+    private void creatingCounts(List<LeipzigCounts> leipzigCountsList, Counts<Link> countsPkw, Counts<Link> countsLkw) {
         Config config = ConfigUtils.createConfig();
         Scenario scenario = ScenarioUtils.createScenario(config);
         MatsimNetworkReader networkReader = new MatsimNetworkReader(scenario.getNetwork());
-        networkReader.readFile(network);
+        networkReader.readFile(network.toString());
         Network network = scenario.getNetwork();
         LeastCostPathCalculator router = generateRouter(network);
-        CoordinateTransformation ct = TransformationFactory.getCoordinateTransformation("EPSG:31468", "EPSG:25832");
         HashMap<String, Link> map = new HashMap<>();
-        ArrayList<leipzigCounts> countList = new ArrayList<>();
+        ArrayList<LeipzigCounts> countList = new ArrayList<>();
 
-        for(leipzigCounts leipzigCounts : leipzigCountsList) {
+        for (LeipzigCounts leipzigCounts : leipzigCountsList) {
             Coord fromCoordOldSys = new Coord(leipzigCounts.getStartNodeCoordX(), leipzigCounts.getStartNodeCoordY());
             Coord toCoordOldSys = new Coord(leipzigCounts.getEndNodeCoordX(), leipzigCounts.getEndNodeCoordY());
-            Coord fromCoord = ct.transform(fromCoordOldSys);
-            Coord toCoord = ct.transform(toCoordOldSys);
+            Coord fromCoord = CT.transform(fromCoordOldSys);
+            Coord toCoord = CT.transform(toCoordOldSys);
             Node fromNode = NetworkUtils.getNearestNode(network, fromCoord);
             Node toNode = NetworkUtils.getNearestNode(network, toCoord);
             LeastCostPathCalculator.Path route = router.calcLeastCostPath(fromNode, toNode, 0.0, null, null);
@@ -162,22 +167,20 @@ public class CreatingCountsFromZaehldaten implements MATSimAppCommand {
                 }
             }
         }
-        writeSafetyFile(countList, network, ct, map);
+        writeSafetyFile(countList, network, map);
 
         CountsWriter writerPkw = new CountsWriter(countsPkw);
         CountsWriter writerLkw = new CountsWriter(countsLkw);
-        writerPkw.write(count + "_Pkw.xml");
-        writerLkw.write(count + "_Lkw.xml");
+        writerPkw.write(count.toString() + "_Pkw.xml");
+        writerLkw.write(count.toString() + "_Lkw.xml");
     }
 
-    private boolean calculateAngle(leipzigCounts leipzigCounts, Link countlink) {
-
-        CoordinateTransformation ct = TransformationFactory.getCoordinateTransformation("EPSG:31468", "EPSG:25832");
+    private boolean calculateAngle(LeipzigCounts leipzigCounts, Link countlink) {
 
         Coord fromCoordOldSys = new Coord(leipzigCounts.getStartNodeCoordX(), leipzigCounts.getStartNodeCoordY());
         Coord toCoordOldSys = new Coord(leipzigCounts.getEndNodeCoordX(), leipzigCounts.getEndNodeCoordY());
-        Coord fromCoord = ct.transform(fromCoordOldSys);
-        Coord toCoord = ct.transform(toCoordOldSys);
+        Coord fromCoord = CT.transform(fromCoordOldSys);
+        Coord toCoord = CT.transform(toCoordOldSys);
 
         double[] v1 = new double[2];
         double[] v2 = new double[2];
@@ -192,21 +195,21 @@ public class CreatingCountsFromZaehldaten implements MATSimAppCommand {
         double betragV1 = Math.sqrt(v1[0] * v1[0] + v1[1] * v1[1]);
         double betragV2 = Math.sqrt(v2[0] * v2[0] + v2[1] * v2[1]);
 
-        double angel = Math.acos(skalarP/(betragV1 * betragV2));
+        double angel = Math.acos(skalarP / (betragV1 * betragV2));
 
-        return angel < Math.PI/6; // 30°
+        return angel < Math.PI / 6; // 30°
 
     }
 
-    private void writeSafetyFile(List<leipzigCounts> leipzigCountsList, Network network, CoordinateTransformation ct, HashMap<String, Link> map) {
-
-        BufferedWriter newwriter = IOUtils.getBufferedWriter("allPoints.txt");
-        try {
+    private void writeSafetyFile(List<LeipzigCounts> leipzigCountsList, Network network, Map<String, Link> map) {
+        CSVPrinter csvPrinter = new CSVPrinter("", CSVFormat.DEFAULT);
+        csvPrinter.printRecords();
+        try (BufferedWriter newwriter = IOUtils.getBufferedWriter("allPoints.txt")) {
             newwriter.write("originalId;oCoordStartX;oCoordStartY;oCoordEndX;oCoordEndY;nCoordStartX;nCoordStartY;nCoordEndX;nCoordEndY");
             newwriter.newLine();
-            for (leipzigCounts lc : leipzigCountsList) {
-                Coord originalFrom = ct.transform(new Coord(lc.getStartNodeCoordX(), lc.getStartNodeCoordY()));
-                Coord originalTo = ct.transform(new Coord(lc.getEndNodeCoordX(), lc.getEndNodeCoordY()));
+            for (LeipzigCounts lc : leipzigCountsList) {
+                Coord originalFrom = CT.transform(new Coord(lc.startNodeCoordX, lc.startNodeCoordY));
+                Coord originalTo = CT.transform(new Coord(lc.endNodeCoordX, lc.endNodeCoordY));
                 newwriter.write(lc.getStreckenNr() + "_" + lc.getStartNodeID() + "_" + lc.getEndNodeID());
                 newwriter.write(";" + originalFrom.getX() + ";" + originalFrom.getY() + ";" + originalTo.getX() + ";" + originalTo.getY());
                 Node fromNode = NetworkUtils.getNearestNode(network, originalFrom);
@@ -215,28 +218,25 @@ public class CreatingCountsFromZaehldaten implements MATSimAppCommand {
                 newwriter.newLine();
                 newwriter.flush();
             }
-            newwriter.close();
         } catch (Exception e) {
-            e.printStackTrace();
+            logger.error("Error when writing points", e);
         }
 
-        BufferedWriter writer2 = IOUtils.getBufferedWriter("lines.txt");
-        try {
-            writer2.write("id;line");
-            writer2.newLine();
-            for ( Map.Entry<String, Link> entry : map.entrySet()) {
+        try (BufferedWriter writer = IOUtils.getBufferedWriter("lines.txt")) {
+            writer.write("id;line");
+            writer.newLine();
+            for (Map.Entry<String, Link> entry : map.entrySet()) {
 //                writer2.write(entry.getKey() + "; LINESTRING (" + entry.getValue().getFromNode().getCoord().getX() + " " + entry.getValue().getFromNode().getCoord().getY() + ", " + entry.getValue().getToNode().getCoord().getX() + " " + entry.getValue().getToNode().getCoord().getY());
-                writer2.write(entry.getKey() + "; LINESTRING (" + entry.getValue().getFromNode().getCoord().getX() + " " + entry.getValue().getFromNode().getCoord().getY() + ", " + entry.getValue().getToNode().getCoord().getX() + " " + entry.getValue().getToNode().getCoord().getY() + ")");
-                writer2.newLine();
-                writer2.flush();
+                writer.write(entry.getKey() + "; LINESTRING (" + entry.getValue().getFromNode().getCoord().getX() + " " + entry.getValue().getFromNode().getCoord().getY() + ", " + entry.getValue().getToNode().getCoord().getX() + " " + entry.getValue().getToNode().getCoord().getY() + ")");
+                writer.newLine();
+                writer.flush();
             }
-            writer2.close();
         } catch (Exception e) {
-            e.printStackTrace();
+            logger.error("Error when writing link ids", e);
         }
     }
 
-    private void fillingCounts(leipzigCounts leipzigCounts, Link countLink, Counts<Link> countsPkw, Counts<Link> countsLkw) {
+    private void fillingCounts(LeipzigCounts leipzigCounts, Link countLink, Counts<Link> countsPkw, Counts<Link> countsLkw) {
         if (countsPkw.getCount(countLink.getId()) != null) {
             Volume x = countsPkw.getCount(countLink.getId()).getVolume(1);
             double c = (x.getValue() + leipzigCounts.getKfz()) / 2;
@@ -263,125 +263,86 @@ public class CreatingCountsFromZaehldaten implements MATSimAppCommand {
                 travelTime);
         return router;
     }
-}
 
-class leipzigCounts {
-    private int streckenNr;
-    private int Kfz;
-    private int Lkw;
-    private int Rad;
-    private int startNodeID;
-    private int endNodeID;
-    private int year;
-    private double startNodeCoordX;
-    private double startNodeCoordY;
-    private double endNodeCoordX;
-    private double endNodeCoordY;
-    private String startName = null;
-    private String endName = null;
+    private static  final class LeipzigCounts {
+        private int streckenNr;
+        private int Kfz;
+        private int Lkw;
+        private int Rad;
+        private int startNodeID;
+        private int endNodeID;
+        private int year;
+        private double startNodeCoordX;
+        private double startNodeCoordY;
+        private double endNodeCoordX;
+        private double endNodeCoordY;
+        private String startName = null;
+        private String endName = null;
 
-    public String getStartName() {
-        return startName;
+        public String getStartName() {
+            return startName;
+        }
+
+        public void setStartName(String startName) {
+            this.startName = startName;
+        }
+
+        public String getEndName() {
+            return endName;
+        }
+
+        public void setEndName(String endName) {
+            this.endName = endName;
+        }
+
+        public int getStreckenNr() {
+            return streckenNr;
+        }
+
+        public void setStreckenNr(int streckenNr) {
+            this.streckenNr = streckenNr;
+        }
+
+        public void setKfz(int kfz) {
+            Kfz = kfz;
+        }
+
+        public void setLkw(int lkw) {
+            Lkw = lkw;
+        }
+
+        public void setRad(int rad) {
+            Rad = rad;
+        }
+
+        public void setStartNodeID(int startNodeID) {
+            this.startNodeID = startNodeID;
+        }
+
+        public void setEndNodeID(int endNodeID) {
+            this.endNodeID = endNodeID;
+        }
+
+        public void setYear(int year) {
+            this.year = year;
+        }
+
+        public void setStartNodeCoordX(double startNodeCoordX) {
+            this.startNodeCoordX = startNodeCoordX;
+        }
+
+        public void setStartNodeCoordY(double startNodeCoordY) {
+            this.startNodeCoordY = startNodeCoordY;
+        }
+
+        public void setEndNodeCoordX(double endNodeCoordX) {
+            this.endNodeCoordX = endNodeCoordX;
+        }
+
+        public void setEndNodeCoordY(double endNodeCoordY) {
+            this.endNodeCoordY = endNodeCoordY;
+        }
+
+
     }
-
-    public void setStartName(String startName) {
-        this.startName = startName;
-    }
-
-    public String getEndName() {
-        return endName;
-    }
-
-    public void setEndName(String endName) {
-        this.endName = endName;
-    }
-
-    public int getStreckenNr() {
-        return streckenNr;
-    }
-
-    public void setStreckenNr(int streckenNr) {
-        this.streckenNr = streckenNr;
-    }
-
-    public void setKfz(int kfz) {
-        Kfz = kfz;
-    }
-
-    public void setLkw(int lkw) {
-        Lkw = lkw;
-    }
-
-    public void setRad(int rad) {
-        Rad = rad;
-    }
-
-    public void setStartNodeID(int startNodeID) {
-        this.startNodeID = startNodeID;
-    }
-
-    public void setEndNodeID(int endNodeID) {
-        this.endNodeID = endNodeID;
-    }
-
-    public void setYear(int year) {
-        this.year = year;
-    }
-
-    public void setStartNodeCoordX(double startNodeCoordX) {
-        this.startNodeCoordX = startNodeCoordX;
-    }
-
-    public void setStartNodeCoordY(double startNodeCoordY) {
-        this.startNodeCoordY = startNodeCoordY;
-    }
-
-    public void setEndNodeCoordX(double endNodeCoordX) {
-        this.endNodeCoordX = endNodeCoordX;
-    }
-
-    public void setEndNodeCoordY(double endNodeCoordY) {
-        this.endNodeCoordY = endNodeCoordY;
-    }
-
-    public int getKfz() {
-        return Kfz;
-    }
-
-    public int getLkw() {
-        return Lkw;
-    }
-
-    public int getRad() {
-        return Rad;
-    }
-
-    public int getStartNodeID() {
-        return startNodeID;
-    }
-
-    public int getEndNodeID() {
-        return endNodeID;
-    }
-
-    public int getYear() {
-        return year;
-    }
-
-    public double getStartNodeCoordX() {
-        return startNodeCoordX;
-    }
-
-    public double getStartNodeCoordY() {
-        return startNodeCoordY;
-    }
-
-    public double getEndNodeCoordX() {
-        return endNodeCoordX;
-    }
-
-    public double getEndNodeCoordY() {
-        return endNodeCoordY;
-    }
-
 }
