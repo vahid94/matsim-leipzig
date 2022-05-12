624f95cb4121eb057e3a5b83d7457b67a0400a9d
simei94 <simonmeinhardt94@gmail.com>
2022-05-12 11:50:17 +0200

    make class runnable

--- src/main/java/org/matsim/run/prepare/DeleteRoutesFromPlans.java
+++ src/main/java/org/matsim/run/prepare/DeleteRoutesFromPlans.java
@@ -2,21 +2,30 @@ package org.matsim.run.prepare;
 
 import org.matsim.api.core.v01.population.*;
 import org.matsim.core.config.Config;
-import org.matsim.core.config.ConfigReader;
 import org.matsim.core.config.ConfigUtils;
 import org.matsim.core.population.PopulationUtils;
 
 public class DeleteRoutesFromPlans {
 //    private static final String inputConfig = "C:/Users/Simon/Desktop/leipzig-v1.0-test.with-drt.config.xml";
-    private static String outputPop = "C:/Users/Simon/Desktop/testPop.xml.gz";
 
-    private Config config;
 
-    public DeleteRoutesFromPlans(Config config) { this.config = config;}
 
-     public Config deleteRoutesFromPlans(Config config) {
+    public static void main(String[] args) {
 
-        Population pop = PopulationUtils.readPopulation(config.plans().getInputFile());
+        String inputPop = "C:/Users/Simon/Desktop/deleteRoutesLeipzig/008.output_plans.xml.gz";
+        String outputPop = "C:/Users/Simon/Desktop/deleteRoutesLeipzig/008.output_plans_cleaned.xml.gz";
+
+        DeleteRoutesFromPlans routeDeleter = new DeleteRoutesFromPlans();
+        routeDeleter.deleteRoutesFromPlans(inputPop, outputPop);
+    }
+
+    public DeleteRoutesFromPlans() { }
+
+     public Config deleteRoutesFromPlans(String inputPop, String outputPop) {
+
+        Population pop = PopulationUtils.readPopulation(inputPop);
+
+        Config config = ConfigUtils.createConfig();
         Population newPop = PopulationUtils.createPopulation(config);
 
         for(Person person  : pop.getPersons().values()) {
@@ -29,7 +38,6 @@ public class DeleteRoutesFromPlans {
         }
         PopulationUtils.writePopulation(newPop, outputPop);
         System.out.println("Pop with deleted routes written to: " + outputPop);
-        System.out.println("Putting newly written pop as input pop");
         System.out.println("size of new population: " + newPop.getPersons().size());
         System.out.println("size of old population: " + pop.getPersons().size());
         config.plans().setInputFile(outputPop);
