b0ecc7e1e5476eae40958428093f1673fa1d3716
Tomkelou <t.stoehr@campus.tu-berlin.de>
2022-05-30 11:59:13 +0200

    Flexa simulation stoptostop count

--- src/main/R/ODAnalysis/Count-link-rides-in-OriginFlexa.R
+++ src/main/R/ODAnalysis/Count-link-rides-in-OriginFlexa.R
@@ -4,64 +4,37 @@ library(dplyr)
 library(terra)
 library(lubridate)
 ######################
-# dies sind die Variablen, die man für den eigenen Gebrauch anpassen muss
-programPath <- "/Users/tomkelouisa/Documents/VSP/Kehlheim/src/main/R/LocDestAnalysis" # hier sollen die Funktionen neareststop.R und countdrivenstops.R liegen
-filePath <- "/Users/tomkelouisa/Documents/VSP/Kehlheimfiles" # hier sollen die beiden folgenden Files liegen
-originfilename <- "VIA_Rides_202106_202201origin.csv" # Filename der Realdaten
-haltestellenFile <- "KEXI_Haltestellen_Liste_Kelheim.csv" # Filename der Haltestellenliste
-startdate <- "2021-07-02"  # wenn das ganze File ausgewertet werden soll, schreibe hier -1 rein
-tage <- 1 # Anzahl an Tagen die ausgewertet werden sollen
-nameAnalyseFile <- paste(startdate , "_Via-Origin-drt-count-Analysis-KEXI.tsv") # Filename des Analyseoutputfiles, in tsv
-csvfilename <- "stop2stopridesVIA.csv"
+# the parameter, which you have to adapt
+programPath <- "/Users/tomkelouisa/Documents/VSP/matsim-leipzig/src/main/R/ODAnalysis" # the function coundrivenstops.R should be there
+filePath <- "/Users/tomkelouisa/Documents/VSP/Leipzig" # the outputfile is going to there and the source file should be there
+originfilename <- "Flexa_Rides_2021.csv" # source file
+#startdate <- "2021-07-02"
+startdate <- -1 #when the whole source file should be operated, so write -1, otherwise write the  date you want to start with
+tage <- 1 # and here the amount of days you want to have starting at the startdate
+nameAnalyseFile <-"stop2stopridesFlexa.tsv" # filename outputfile
+#csvfilename <- "stop2stopridesVIA.csv"
 setwd(programPath)
-source("neareststop.R")
+
 source("countdrivenstops.R")
 
 
 
 setwd(filePath)
-# Daten von VIA einlesen
-viaDataframe <- read.csv(originfilename, stringsAsFactors = FALSE, header = TRUE, encoding = "UTF-8", sep= ";")
+#read source data
+flexaDataframe <- read.csv(originfilename, stringsAsFactors = FALSE, header = TRUE, encoding = "UTF-8", sep= ";")
 
-viaDataframe <- viaDataframe %>% mutate(Actual.DO.time = ymd_hms(Actual.DO.time))
+flexaDataframe <- flexaDataframe %>% mutate(actual_departure_time = ymd_hms(actual_departure_time))
 
 if (startdate!=-1){
+  nameAnalyseFile <- paste(startdate , "_stop2stopridesFlexa.tsv")
   d <- interval(ymd(startdate),ymd(startdate)+days(x=tage))
-  viaDataframe <- viaDataframe %>% filter(Actual.DO.time %within% d)
+  flexaDataframe <- flexaDataframe %>% filter(actual_departure_time %within% d)
 }
 
 
 
-if(("fromstopID" %in% colnames(viaDataframe))==FALSE){
-  #VSP-stops müssen den VIA Daten hinzugefügt werd
-  ######################################
-
-  # Daten der jeweiligen Haltestellen einlesen
-
-  stops <- read.csv(haltestellenFile, stringsAsFactors = FALSE, header = TRUE, encoding = "UTF-8",sep= ";")
-
-
-
-  ########################################
-  # fügt zu den VIA Daten den Haltestellenwert VSP hinzu
-
-  viaDataframe <- viaDataframe  %>%
-    rowwise() %>%
-    #mutate(fromstopID=as.integer(neareststop(Origin.Longitude, Origin.latitude,"Stop.ID")))
-    mutate(fromstopID=as.integer(neareststop(Origin.Longitude, Origin.latitude,"Haltestellen.Nr.")))
-
-  viaDataframe <- viaDataframe  %>%
-    rowwise() %>%
-    #mutate(tostop_ID = as.integer(neareststop(Destination.Longitude, Destination.latitude,"Stop.ID")))
-    mutate(tostop_ID=as.integer(neareststop(Destination.Longitude, Destination.latitude,"Haltestellen.Nr.")))
-
-  setwd(filePath)
-
-  #write.csv2(viaDataframe, "VIA_Rides_202106_202201neu.csv",quote = FALSE)
-
-}
-# zählt die Anzahl der Fahrten von einem Stop zum nächsten
-countdrivenlinks(viaDataframe,"fromstopID","tostop_ID",nameAnalyseFile)
+# counts the amount of stop-to-stop connections
+countdrivenlinks(flexaDataframe, 'requested_origin_assigned_flexa_stop', 'requested_destination_assigned_flexa_stop', nameAnalyseFile)
 
 
 
--- src/main/R/ODAnalysis/countdrivenstops.R
+++ src/main/R/ODAnalysis/countdrivenstops.R
@@ -1,20 +1,17 @@
 countdrivenlinks <- function(movements, fromstopID,tostop_ID,tsvname){
 
-  sortedMovement <- movements[order(movements$fromstopID,movements$tostop_ID), ]
+  sortedMovement <- movements[order(movements$requested_origin_assigned_flexa_stop,movements$requested_destination_assigned_flexa_stop), ]
 
   #erstellt Vektoren / vll noch eone schönere Lösung möglich
-  laenge <- length(movements$fromstopID)
+  laenge <- length(movements$requested_origin_assigned_flexa_stop)
   fromLink <-character(0)
   toLink <- character(0)
   anzahlFahrten <- character(0)
   fromstopIds <- character(0)
   tostopIds <- character(0)
-  tolat <- character(0)
-  tolon <- character(0)
-  fromlat <- character(0)
-  fromlon <- character(0)
 
-  Fahrten <- 1 # gibt Anzahl der Fahrten zwischen zwei Links an
+
+  Fahrten <- 1 # counter of Trips from one to another stop
   connection <- c(sortedMovement[1,fromstopID],sortedMovement[1,tostop_ID]) # Vektor mit einem from und einem to Link drin
 
   #Iteration durch sorted Movement, wobei paarweise die Tuple (hier Vektoren, connection und newConnection) verglichen werden
@@ -31,10 +28,7 @@ countdrivenlinks <- function(movements, fromstopID,tostop_ID,tsvname){
       Fahrten <- 1
       fromstopIds <- c(fromstopIds,as.character(connection[1]))
       tostopIds <- c(tostopIds,as.character(connection[2]))
-      tolat <- c(tolat,as.character(stops$lat[which(stops$Haltestellen.Nr.==connection[2],arr.ind=FALSE)]))
-      tolon <- c(tolon,as.character(stops$lon[which(stops$Haltestellen.Nr.==connection[2],arr.ind=FALSE)]))
-      fromlat <- c(fromlat,as.character(stops$lat[which(stops$Haltestellen.Nr.==connection[1],arr.ind=FALSE)]))
-      fromlon <- c(fromlon,as.character(stops$lon[which(stops$Haltestellen.Nr.==connection[1],arr.ind=FALSE)]))
+
 
       connection <- newConnection
 
@@ -48,17 +42,13 @@ countdrivenlinks <- function(movements, fromstopID,tostop_ID,tsvname){
   anzahlFahrten <- c(anzahlFahrten,Fahrten)
   fromstopIds <- c(fromstopIds,as.character(connection[1]))
   tostopIds <- c(tostopIds,as.character(connection[2]))
-  tolat <- c(tolat,as.character(stops$lat[which(stops$Haltestellen.Nr.==connection[2],arr.ind=FALSE)]))
-  tolon <- c(tolon,as.character(stops$lon[which(stops$Haltestellen.Nr.==connection[2],arr.ind=FALSE)]))
-  fromlat <- c(fromlat,as.character(stops$lat[which(stops$Haltestellen.Nr.==connection[1],arr.ind=FALSE)]))
-  fromlon <- c(fromlon,as.character(stops$lon[which(stops$Haltestellen.Nr.==connection[1],arr.ind=FALSE)]))
-  #fromstopIds <- c(fromstopIds,as.character(stops[,Stop.ID[which(stops[,Stop.ID==connection[1]],arr.ind=FALSE)]]))
-  #tostopIds <- c(tostopIds,as.character(stops[,Stop.ID[which(stops[,Stop.ID==connection[2]],arr.ind=FALSE)]]))
+
+
 
 
   # in Datadfame speichern und als csv Datei abspeichern
 
-  class.df <- data.frame(fromstopIds,tostopIds,fromlat,fromlon,tolat,tolon,anzahlFahrten,stringsAsFactors = FALSE)
+  #class.df <- data.frame(fromstopIds,tostopIds,fromlat,fromlon,tolat,tolon,anzahlFahrten,stringsAsFactors = FALSE)
   class.smalldf <- data.frame(fromstopIds,tostopIds,anzahlFahrten)
 
 
@@ -66,7 +56,7 @@ countdrivenlinks <- function(movements, fromstopID,tostop_ID,tsvname){
 
   setwd(filePath)
 
-  write.csv2(class.smalldf,csvfilename,quote=FALSE, row.names=FALSE)
-  write.table(class.df,tsvname,quote=FALSE, sep="\t",col.names = NA,row.names = TRUE)
+  #write.csv2(class.smalldf,csvfilename,quote=FALSE, row.names=FALSE)
+  write.table(class.smalldf,tsvname,quote=FALSE, sep="\t",row.names = F)
 
 }
