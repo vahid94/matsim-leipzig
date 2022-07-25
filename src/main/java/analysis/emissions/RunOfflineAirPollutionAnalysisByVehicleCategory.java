9d25d039f0c6e3249b21c6527d536af4b0654076
Ruan J. Gräbe <rgraebe@vsp.tu-berlin.de>
2022-07-25 13:46:15 +0200

    Encrypted files in main method

--- src/main/java/analysis/emissions/RunOfflineAirPollutionAnalysisByVehicleCategory.java
+++ src/main/java/analysis/emissions/RunOfflineAirPollutionAnalysisByVehicleCategory.java
@@ -25,7 +25,6 @@ import org.apache.logging.log4j.Logger;
 import org.matsim.api.core.v01.Id;
 import org.matsim.api.core.v01.Scenario;
 import org.matsim.api.core.v01.network.Link;
-import org.matsim.api.core.v01.network.Network;
 import org.matsim.contrib.emissions.EmissionModule;
 import org.matsim.contrib.emissions.HbefaVehicleCategory;
 import org.matsim.contrib.emissions.Pollutant;
@@ -43,8 +42,6 @@ import org.matsim.core.events.EventsUtils;
 import org.matsim.core.events.MatsimEventsReader;
 import org.matsim.core.events.algorithms.EventWriterXML;
 import org.matsim.core.scenario.ScenarioUtils;
-import org.matsim.core.utils.io.CipherUtils;
-import org.matsim.core.utils.io.IOUtils;
 import org.matsim.vehicles.EngineInformation;
 import org.matsim.vehicles.VehicleType;
 import org.matsim.vehicles.VehicleUtils;
@@ -54,18 +51,17 @@ import java.io.File;
 import java.io.FileWriter;
 import java.io.IOException;
 import java.nio.file.Path;
-import java.util.Arrays;
 import java.util.Map;
 
 import static org.matsim.application.ApplicationUtils.globFile;
 
-// TODO local file usage vs encrypted (kmt)
 /**
  * This analysis class requires two parameters as arguments: <br>
- * (1) the root/working directory, and <br>
- * (2) the path to your (SVN) folder containing the exported HBEFA files.
+ * (1) the run directory, and <br>
+ * (2) the password (passed as environment variable in your IDE
+ * and/or on the server) to access the encrypted files on the public-svn.
  *
- * @author rgraebe
+ * @author Ruan J. Gräbe (rgraebe)
 */
 
 public class RunOfflineAirPollutionAnalysisByVehicleCategory {
@@ -88,26 +84,21 @@ public class RunOfflineAirPollutionAnalysisByVehicleCategory {
 	}
 	
 	public static void main(String[] args) {
-		
-		if (args.length == 2) {
+
+		if (args.length == 1) {
 			String runDirectory = args[0];
-			String svnDirectory = args[1];
 			if (!runDirectory.endsWith("/")) runDirectory = runDirectory + "/";
-			if (!svnDirectory.endsWith("/")) svnDirectory = svnDirectory + "/";
 
-			// based on the simulation output available within this project
-//			final String runDirectory = "output/it-1pct/";
-			final String runId = "leipzig-25pct";
+			final String runId = "leipzig-25pct"; // based on the simulation output available in this project
 
-			// Currently (05/22), these files are (only) available in the shared-svn
-			final String hbefaFileCold = "projects/matsim-germany/hbefa/hbefa-files/v4.1/EFA_ColdStart_Vehcat_2020_Average_withHGVetc.csv";
-			final String hbefaFileWarm = "projects/matsim-germany/hbefa/hbefa-files/v4.1/EFA_HOT_Vehcat_2020_Average.csv";
+			String hbefaFileWarm = "https://svn.vsp.tu-berlin.de/repos/public-svn/3507bb3997e5657ab9da76dbedbb13c9b5991d3e/0e73947443d68f95202b71a156b337f7f71604ae/7eff8f308633df1b8ac4d06d05180dd0c5fdf577.enc";
+			String hbefaFileCold = "https://svn.vsp.tu-berlin.de/repos/public-svn/3507bb3997e5657ab9da76dbedbb13c9b5991d3e/0e73947443d68f95202b71a156b337f7f71604ae/ColdStart_Vehcat_2020_Average_withHGVetc.csv.enc";
 
 			RunOfflineAirPollutionAnalysisByVehicleCategory analysis = new RunOfflineAirPollutionAnalysisByVehicleCategory(
 					runDirectory,
 					runId,
-					svnDirectory + hbefaFileWarm,
-					svnDirectory + hbefaFileCold,
+					hbefaFileWarm,
+					hbefaFileCold,
 					runDirectory + "emission-analysis-offline");
 			try {
 				analysis.run();
@@ -116,7 +107,7 @@ public class RunOfflineAirPollutionAnalysisByVehicleCategory {
 			}
 
 		} else {
-			throw new RuntimeException("Please set the two (run and input) directory paths. \nCheck the class description for more details. Aborting...");
+			throw new RuntimeException("Please set the run directory path and/or password. \nCheck the class description for more details. Aborting...");
 		}
 	}
 
