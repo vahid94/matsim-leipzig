8b9b7b0ce2975d2afac8f2e0df183685adedfb94
hzoerner <h.zoerner@campus.tu-berlin.de>
2022-10-05 15:11:30 +0200

    moved symlog_trans and scatter plot to library added analysis of dtv estimation quality

--- counts-with-library.R
+++ counts-with-library.R
@@ -6,38 +6,6 @@ library(tidyverse)
 library(scales)
 library(geomtextpath)
 
-symlog_trans <- function(base = 10, thr = 1, scale = 1){
-  trans <- function(x)
-    ifelse(abs(x) < thr, x, sign(x) * 
-             (thr + scale * suppressWarnings(log(sign(x) * x / thr, base))))
-  
-  inv <- function(x)
-    ifelse(abs(x) < thr, x, sign(x) * 
-             base^((sign(x) * x - thr) / scale) * thr)
-  
-  breaks <- function(x){
-    sgn <- sign(x[which.max(abs(x))])
-    if(all(abs(x) < thr))
-      pretty_breaks()(x)
-    else if(prod(x) >= 0){
-      if(min(abs(x)) < thr)
-        sgn * unique(c(pretty_breaks()(c(min(abs(x)), thr)),
-                       log_breaks(base)(c(max(abs(x)), thr))))
-      else
-        sgn * log_breaks(base)(sgn * x)
-    } else {
-      if(min(abs(x)) < thr)
-        unique(c(sgn * log_breaks()(c(max(abs(x)), thr)),
-                 pretty_breaks()(c(sgn * thr, x[which.min(abs(x))]))))
-      else
-        unique(c(-log_breaks(base)(c(thr, -x[1])),
-                 pretty_breaks()(c(-thr, thr)),
-                 log_breaks(base)(c(thr, x[2]))))
-    }
-  }
-  trans_new(paste("symlog", thr, base, scale, sep = "-"), trans, inv, breaks)
-}
-
 COUNTS <- "output/leipzig-v1.1-counts_Pkw.xml"
 NETWORK <- "C:/Users/ACER/Desktop/Uni/VSP/matsim-leipzig/output/leipzig-v1.1-network-with-pt-drt.xml"
 
@@ -47,54 +15,12 @@ linkStats = list(readLinkStats(runId = "v1.0-run039", file = "C:/Users/ACER/Desk
 counts <- readCounts(COUNTS)
 network <- loadNetwork(NETWORK)
 
-join <- mergeCountsAndLinks(counts = counts, linkStats = linkStats, network = network,
+join <- mergeCountsAndLinks(counts = counts, linkStats  = linkStats, network = network,
                             networkModes = c("car"))
 
 #### VIA-styled scatterplot ####
-line.size <- 0.7
 
-threshold <- 100
-
-x <- seq(threshold, round(max(join$count), -2), 10)
-
-upper.limit <- data.frame(x = x,
-                          y = 1.2 * x)
-
-lower.limit <- data.frame(x = x,
-                          y = 0.8 * x)
-
-middle.line <- data.frame(x = x,
-                          y = x)
-
-ggplot(join, aes(x = count, y = volume, color = type)) +
-  
-  geom_point() +
-  
-  geom_line(data = middle.line, mapping = aes(x, y), size = line.size, linetype = "dashed", color = "grey60") +
-  
-  geom_line(data = upper.limit, mapping = aes(x, y), color = "black", size = line.size + 0.1) +
-  
-  geom_line(data = lower.limit, mapping = aes(x, y), color = "black", size = line.size + 0.1) +
-  
-  geom_vline(xintercept = threshold, linetype = "dashed") +
-  
-  geom_textvline(xintercept = threshold, label = "x = 100", linetype = "dashed", vjust = -0.3) +
-  
-  scale_x_continuous(trans = symlog_trans(thr = 100, scale = 1000), breaks = c(0, 300, 1000, 3000, 10000, 30000, 100000)) + 
-  
-  scale_y_continuous(trans = "log10", breaks = c(3, 10, 30, 100, 300, 1000, 3000, 10000, 30000)) + 
-  
-  facet_wrap(.~ src) +
-  
-  labs(x = "Daily traffic volume from Count Stations", y = "Daily traffic volume from MATSim Data", color = "Road type:") +
-  
-  theme_bw() +
-  
-  theme(legend.position = "bottom", panel.background = element_rect(fill = "grey90"),
-        panel.grid = element_line(colour = "white"))
-
-rm(join.scatterplot, lower.limit, upper.limit, middle.line)
-
+createCountScatterPlot(joinedFrame = join)
 
 #### Analysis of DTV distribution ####
 join.dtv.distribution <- processLinkStatsDtvDistribution(joinedFrame = join)
@@ -111,5 +37,22 @@ ggplot(join.dtv.distribution, aes(x = traffic_bin, y = share, fill = type)) +
   
   theme(legend.position = "none", axis.text.x = element_text(angle = 90))
 
+rm(join.dtv.distribution)
+
 
 #### Analysis of Estimation Quality ####
+
+join.est.quality <- processDtvEstimationQuality(joinedFrame = join, aggr = T) %>%
+  filter(!type %in% c("residential", "unclassified", NA))
+
+ggplot(join.est.quality, aes(estimation, share, fill = type)) +
+  
+  geom_col() +
+  
+  labs(y = "Share", x = "Quality category") +
+  
+  facet_grid(src ~ type) +
+  
+  theme_bw() +
+  
+  theme(legend.position = "none", axis.text.x = element_text(angle = 90))
