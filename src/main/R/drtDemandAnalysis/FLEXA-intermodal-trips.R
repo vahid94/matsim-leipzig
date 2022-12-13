e39bfda3d6d1f08f6d444ca5d9159bf4b3f1894f
hzoerner <h.zoerner@campus.tu-berlin.de>
2022-12-13 13:50:14 +0100

    added analysis code and plot export

--- src/main/R/drtDemandAnalysis/FLEXA-intermodal-trips.R
+++ src/main/R/drtDemandAnalysis/FLEXA-intermodal-trips.R
@@ -39,3 +39,62 @@ for(file in files){
 }
 
 ssh_disconnect(connection)
+rm(connection, file, files, pattern.legs, t, target.dir, target.files, PROJECT_DIR, REMOTE_DIR)
+
+
+####### ANALYSIS #######
+
+TRIP_FILES <- list.files(path = FILES_DIR, pattern = pattern.trips)
+trips.compare <- tibble(main_mode = character(),
+                        main_mode_ger = character(),
+                        n = numeric(),
+                        share = double(),
+                        scenario = character())
+
+for(file in TRIP_FILES){
+  
+  split.1 <- unlist(str_split(file, pattern = "case"))[2]
+  scenario <- unlist(str_split(split.1, pattern = "[.]"))[1]
+  
+  TRIPS <- paste0(FILES_DIR, "/", file)
+  
+  trips <- read_csv2(TRIPS)
+  
+  trips.drt <- trips %>%
+    filter(str_detect(string = modes, pattern = "drt"))
+  
+  trips.drt %>%
+    filter(main_mode != "drt") %>%
+    filter(str_detect(string = modes, pattern = "drt"))
+  
+  drt.sum <- trips.drt %>%
+    group_by(main_mode) %>%
+    summarise(n = n()) %>%
+    ungroup() %>%
+    mutate(share = n / sum(n),
+           share = round(share, 3)) %>%
+    mutate(main_mode_ger = ifelse(main_mode == "drt", "DRT", "ÖPNV"),
+           scenario = scenario)
+  
+  trips.compare = bind_rows(trips.compare, drt.sum)
+}
+
+ggplot(data = trips.compare, aes(main_mode_ger, share, fill = main_mode)) +
+  
+  geom_col(color = "black") +
+  
+  geom_text(aes(label = paste("n =", n)), vjust = -1, size = 3.5) +
+  
+  facet_wrap(scenario ~.) +
+  
+  coord_cartesian(ylim = c(0, 1.1)) +
+  
+  scale_y_continuous(breaks = seq(0, 1, 0.2)) +
+  
+  labs(x = "Hauptverkehrsmittel", y = "Anteil", fill = "Hauptverkehrsmittel", title = "Hauptverkehrsmittelwahl unter Nutztung von FLEXA") +
+  
+  theme_bw() +
+  
+  theme(legend.position = "none")
+
+ggsave(plot = last_plot(), filename = paste0(FILES_DIR, "/", "Hauptverkehrsmittelwahl_FLEXA.jpg"))
