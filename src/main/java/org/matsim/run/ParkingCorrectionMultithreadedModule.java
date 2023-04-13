5c4b0e30eaa522cf7c9aad570290ed57287463b0
Kai Nagel <nagel@vsp.tu-berlin.de>
2023-04-13 10:53:18 +0200

    suggestion for first steps

--- src/main/java/org/matsim/run/ParkingCorrectionMultithreadedModule.java
+++ src/main/java/org/matsim/run/ParkingCorrectionMultithreadedModule.java
@@ -2,19 +2,28 @@ package org.matsim.run;
 
 import org.apache.logging.log4j.LogManager;
 import org.apache.logging.log4j.Logger;
+import org.matsim.api.core.v01.TransportMode;
 import org.matsim.api.core.v01.population.Plan;
+import org.matsim.api.core.v01.population.PlanElement;
+import org.matsim.api.core.v01.population.Population;
+import org.matsim.api.core.v01.population.PopulationFactory;
 import org.matsim.core.config.groups.GlobalConfigGroup;
+import org.matsim.core.population.PopulationUtils;
 import org.matsim.core.population.algorithms.PlanAlgorithm;
 import org.matsim.core.replanning.modules.AbstractMultithreadedModule;
+import org.matsim.core.router.TripRouter;
 import org.matsim.core.router.TripStructureUtils;
 
+import java.util.ArrayList;
 import java.util.List;
 
 class ParkingCorrectionMultithreadedModule extends AbstractMultithreadedModule {
 	private static final Logger log = LogManager.getLogger(ParkingCorrectionMultithreadedModule.class );
+	private final PopulationFactory pf;
 
-	public ParkingCorrectionMultithreadedModule( GlobalConfigGroup globalConfigGroup ){
+	public ParkingCorrectionMultithreadedModule( GlobalConfigGroup globalConfigGroup, PopulationFactory pf ){
 		super( globalConfigGroup );
+		this.pf = pf;
 	}
 	@Override public PlanAlgorithm getPlanAlgoInstance(){
 		return new PlanAlgorithm(){
@@ -22,22 +31,39 @@ class ParkingCorrectionMultithreadedModule extends AbstractMultithreadedModule {
 
 				List<TripStructureUtils.Trip> trips = TripStructureUtils.getTrips( plan.getPlanElements() );
 
-				for( TripStructureUtils.Trip trip : trips ){
-					if ( isRouted(trip ) ) {
+				for( TripStructureUtils.Trip oldTrip : trips ){
+					if ( isRouted(oldTrip ) ) {
 						continue;;
 					}
 					// else check if parking is affected etc.
 
-					trip.getOriginActivity();
+					oldTrip.getOriginActivity();
 
-					trip.getDestinationActivity();
-
-					Plan newPlan = null ;
-
-					// ...
+					oldTrip.getDestinationActivity();
 
 
-					plan = newPlan ; /// does not work
+					List<PlanElement> newTrip = new ArrayList<>();
+
+//					newTrip.add( oldTrip.getOriginActivity() );
+
+					newTrip.add( pf.createLeg( TransportMode.walk ) );
+
+					newTrip.add( pf.createInteractionActivityFromLinkId( ... ) );
+
+					newTrip .add( pf.createLeg( TransportMode.car );
+
+					newTrip.add( pf.createInteractionActivityFromLinkId( ... ) );
+
+					newTrip.add( pf.createLeg(  ))
+
+//					newTrip.add( oldTrip.getDestinationActivity() );
+
+
+					TripRouter.insertTrip(
+							plan,
+							oldTrip.getOriginActivity(),
+							newTrip,
+							oldTrip.getDestinationActivity() );
 
 				}
 
