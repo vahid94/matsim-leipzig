5b0374dad202e7d9b0e3bce0eb8957aba115abf6
mkreuschner <64031262+mkreuschner@users.noreply.github.com>
2023-04-13 10:20:36 +0200

    Add files via upload

--- src/main/R/masterscript.R
+++ src/main/R/masterscript.R
@@ -15,38 +15,48 @@ library(networkD3)
 library(alluvial)
 library(ggalluvial)
 library(stringr)
+library(data.table)
 print("#### Libraries geladen! ####")
+################################################################################ CASES #### please put (1=yes/0=no) for analyses 
+#base-case = 0
+#carfree-area-90 = 0
+#carfree-area-95 = 0
+#carfree-area-99 = 0
+#drt-outskirts = 0
+#drt-whole-city = 0
+#slow-speed-absolute = 0
+#slow-speed-relative = 0
+#combined_scenarioA = 0
 ################################################################################ INPUT ####
-workingDirectory <- "D:/VSP_Berlin/Leipzig/Car_free_areas"
-setwd(workingDirectory)
-################################################################################ fill out paths
-runID = "Leipzig_speed_20relativ_25pct"                                        
-network <- "base/leipzig-25pct-base.output_network.xml.gz"
-base_persons_path <- "matsim_output/base/leipzig-flexa-25pct-scaledFleet-base_noDepot.output_persons.csv.gz"
-base_trips_path <- "matsim_output/base/leipzig-flexa-25pct-scaledFleet-base_noDepot.output_trips.csv.gz"
-base_legs_path <- "matsim_output/base/leipzig-flexa-25pct-scaledFleet-base_noDepot.output_legs.csv.gz"
-#base_traffic <- "traffic_output_base.csv"
-#base_emission <- ""
-scenario_persons_path <- "matsim_output/carfree90pct/leipzig-flexa-25pct-scaledFleet-carfree90pct_noDepot.output_persons.csv.gz"
-scenario_trips_path <- "20relativ/leipzig-drt-20relativ-25pct.output_trips.csv.gz"
-scenario_legs_path <- "20relativ/leipzig-drt-20relativ-25pct.output_legs.csv.gz"
-#scenario_traffic <- "..."
-#scenario_emission <- "..."
+publicSVN = "/Users/mkreuschnervsp/Desktop/git/public-svn/matsim/scenarios/countries/de/leipzig/projects/namav/"
+#local = /Users/mkreuschnervsp/Desktop/VSP_projects/02_NaMAV
+
+runID = "carfree-area-90"                                        
+network <- paste(publicSVN,"base-case/leipzig-25pct-base.output_network.xml.gz")
+CRS <- 25832
+
+scenario_run_path <- paste(publicSVN,runID)
+
+#base path nur für Sankey und Winner/Loser Analysis
+base_run_path <- "/Users/mkreuschnervsp/Desktop/git/public-svn/matsim/scenarios/countries/de/leipzig/projects/namav/base-case/"
 
 
-region_shp_path <- "../shapefiles/Leipzig_puffer.shp"
-city_shp_path <- "../shapefiles/Leipzig_stadt.shp"
+region_shp_path <- "/Users/mkreuschnervsp/Desktop/VSP_projects/02_NaMAV/R/shapefiles/Leipzig_puffer.shp"
+city_shp_path <- "/Users/mkreuschnervsp/Desktop/VSP_projects/02_NaMAV/R/shapefiles/Leipzig_stadt.shp"
+area_shp_path <- "/Users/mkreuschnervsp/Desktop/VSP_projects/02_NaMAV/R/shapefiles/Zonen90_update.shp"
+
 
 print("#### Inputspath definiert! ####")
 ################################################################################ OUTPUT ####
 
-outputDirectoryGeneral <-  paste(workingDirectory, "/output_general", sep = "") # the plots are going to be saved here
-if(!file.exists(outputDirectoryGeneral)){
+outputDirectoryBase <-  "/Users/mkreuschnervsp/Desktop/VSP_projects/02_NaMAV/R/base-analysis-R" # the plots are going to be saved here
+if(!file.exists(outputDirectoryBase)){
   print("creating analysis sub-directory")
-  dir.create(outputDirectoryGeneral)  
+  dir.create(outputDirectoryBase)  
 }
-
-outputDirectoryScenario <-  paste(workingDirectory, "/output_scenario", sep = "") # the plots are going to be saved here
+#/Users/mkreuschnervsp/Desktop/git/public-svn/matsim/scenarios/countries/de/leipzig/projects/namav/",runID,"/analysis/analysis-R
+outputDirectoryScenario <-  "/Users/mkreuschnervsp/Desktop/VSP_projects/02_NaMAV/R/policy-analysis-R"
+  #paste(scenario_run_path, "analysis/analysis-R", sep = "") # the plots are going to be saved here
 if(!file.exists(outputDirectoryScenario)){
   print("creating analysis sub-directory")
   dir.create(outputDirectoryScenario)  
@@ -54,43 +64,67 @@ if(!file.exists(outputDirectoryScenario)){
 
 print("#### Output folder geladen! ####")
 ################################################################################ ANALYSIS ####
+# PLEASE put (1=yes/0=no) for certain analysis 
 
-x_average_traveled_distance_trips =  1
-x_average_traveled_distance_legs =   0
+#### #1.1 Modal Split COUNTS - trips based
+  x_ms_trips_count =          1
+#### #1.2 Modal Split DISTANCE - trips based
+  x_ms_trips_distance =       1
+#### #1.3 Modal Split COUNTS- legs based
+  x_ms_legs_count =           0
+#### #1.4 Modal Split DISTANCE - legs based  
+  x_ms_legs_distance =        0
 
-x_average_time_trips =      1
-x_average_time_legs =       0
+#### #2.1 Modal Shift - trips based
+  x_sankey_diagram = 1
 
-x_average_traveled_speed_trips =    1
-x_average_beeline_speed_trips =     1
+#### #3.1 Distances TRAVELED - trips based
+  x_average_traveled_distance_trips =   1
+#### #3.2 Distances EUCLIDEAN - trips based
+  x_average_euclidean_distance_trips =  1
+#### #3.3 PKM - trips based  
+  x_personen_km_trips =                 0
+#### #3.4 Distances TRAVELED - legs based
+  x_average_traveled_distance_legs =    0
+#### #3.5 Distances EUCLIDEAN - legs based  
+  x_average_euclidean_distance_legs =   0
+#### #3.6 PKM - legs based
+  x_personen_km_legs =                  1
 
-x_personen_km_trips =       0
-x_personen_km_legs =        1
+#### #4.1 Time Traveled - trips based
+  x_average_time_trips =      1
+#### #4.2 Time Traveled - legs based  
+  x_average_time_legs =       0
+#### #4.3 ph - trips based    
+  x_personen_h_trips =        0
+#### #4.4 ph - legs based 
+  x_personen_h_legs =         1
 
-x_personen_h_trips =        0
-x_personen_h_legs =         1
+#### #5.1 Speed TRAVELED - trips based
+  x_average_traveled_speed_trips =    1
+#### #5.2 Speed BEELINE - trips based
+  x_average_beeline_speed_trips =     1
 
-x_ms_trips_count =          1
-x_ms_trips_distance =       1
+#### #6.1 Traffic Volumes  
+  #x_traffic = 0
 
-x_ms_legs_count =           0
-x_ms_legs_distance =        0
+#### #7.1 Emissions Analysis  
+  #x_emissions = 0  
+  
+#### #8.1 Winner/Loser Analysis
+  x_winner_loser = 1
 
 #x_distance_distribution_trips = 1
 #x_distance_distribution_legs =  0
   
-x_sankey_diagram = 1
 
-#x_emissions = 0
 
-#x_traffic = 0
 
-x_winner_loser = 1
-
+  
 print("#### Auswahl getroffen! ####")
 ################################################################################ SOURCE ####
 
-source("masteranalyse2.R")
+source("/Users/mkreuschnervsp/Desktop/R_Studio/mastersolver.R")
 
 print("#### Masterscript fertig! ####")
 
