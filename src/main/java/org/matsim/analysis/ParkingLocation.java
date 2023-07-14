e7c75e218d610b8e995fc29d2514f3c63c1e7769
GregorRyb <rybczak@vsp.tu-berlin.de>
2023-07-14 10:50:31 +0200

    write out agents who perform parking activities

--- src/main/java/org/matsim/analysis/ParkingLocation.java
+++ src/main/java/org/matsim/analysis/ParkingLocation.java
@@ -7,11 +7,14 @@ import org.matsim.api.core.v01.events.handler.ActivityStartEventHandler;
 import org.matsim.api.core.v01.network.Link;
 import org.matsim.api.core.v01.network.Network;
 import org.matsim.api.core.v01.population.Person;
+import org.matsim.api.core.v01.population.Population;
 import org.matsim.application.MATSimAppCommand;
 import org.matsim.core.api.experimental.events.EventsManager;
+import org.matsim.core.config.ConfigUtils;
 import org.matsim.core.events.EventsUtils;
 import org.matsim.core.events.MatsimEventsReader;
 import org.matsim.core.network.NetworkUtils;
+import org.matsim.core.population.PopulationUtils;
 import org.matsim.core.utils.io.IOUtils;
 import org.matsim.vehicles.Vehicle;
 import picocli.CommandLine;
@@ -40,15 +43,28 @@ public class ParkingLocation implements MATSimAppCommand {
 	public Integer call() throws Exception {
 		Path eventsPath = globFile(directory, "*output_events.*");
 		Path networkPath = globFile(directory, "*output_network.*");
+		Path popPath = globFile(directory, "*output_plans.*");
 		EventsManager manager = EventsUtils.createEventsManager();
 		Network network = NetworkUtils.readNetwork(String.valueOf(networkPath));
 		List<ParkingData> listOfParkingActivities = new ArrayList<>();
-		manager.addHandler(new ParkingActivites(listOfParkingActivities, network));
+		List<Id<Person>> listOfRelevantPersons = new ArrayList<>();
+		manager.addHandler(new ParkingActivites(listOfParkingActivities, network, listOfRelevantPersons));
 		manager.initProcessing();
 		MatsimEventsReader matsimEventsReader = new MatsimEventsReader(manager);
 		matsimEventsReader.readFile(eventsPath.toString());
 		manager.finishProcessing();
 		writeResults(directory, listOfParkingActivities);
+
+		if (listOfRelevantPersons.size()>0) {
+			Population population = PopulationUtils.readPopulation(String.valueOf(popPath));
+			Population reducedPop = PopulationUtils.createPopulation(ConfigUtils.createConfig());
+			for (Person p: population.getPersons().values()) {
+				if (listOfRelevantPersons.contains(p.getId())) {
+					reducedPop.addPerson(p);
+				}
+			}
+			PopulationUtils.writePopulation(reducedPop,directory+"/reducedPlans.xml");
+		}
 
 		return null;
 	}
@@ -57,10 +73,12 @@ public class ParkingLocation implements MATSimAppCommand {
 	private static class ParkingActivites implements ActivityStartEventHandler {
 		private final List<ParkingData> listOfParkingData;
 		private final Network network;
+		private final List<Id<Person>> listOfRelevantPersons;
 
-		ParkingActivites(List<ParkingData> listOfParkingData, Network network) {
+		ParkingActivites(List<ParkingData> listOfParkingData, Network network, List<Id<Person>> listOfRelevantPersons ) {
 			this.listOfParkingData = listOfParkingData;
 			this.network = network;
+			this.listOfRelevantPersons = listOfRelevantPersons;
 		}
 
 		@Override
@@ -69,17 +87,21 @@ public class ParkingLocation implements MATSimAppCommand {
 				Link l =  network.getLinks().get(activityStartEvent.getLinkId());
 				ParkingData pd = new ParkingData(activityStartEvent.getPersonId(), l.getCoord(), activityStartEvent.getLinkId());
 				listOfParkingData.add(pd);
+
+				if(l.getAttributes().getAttribute("linkParkingType")!= null) {
+					listOfRelevantPersons.add(activityStartEvent.getPersonId());
+				}
 			}
 		}
 	}
 
 	private static void writeResults(Path outputFolder, List<ParkingData> listOfParkingData) throws IOException {
 		BufferedWriter writer = IOUtils.getBufferedWriter(outputFolder.toString() + "/parkingActivities.tsv");
-		writer.write("x" + "\t" + "y" + "\t"  + "linkId" );
+		writer.write("personId" + "\t" + "x" + "\t" + "y" + "\t"  + "linkId" );
 		writer.newLine();
 		for (int i = 0; i < listOfParkingData.size(); i++) {
 			ParkingData pd = listOfParkingData.get(i);
-			writer.write(pd.coord.getX() + "\t" + pd.coord.getY() + "\t" + pd.linkId);
+			writer.write(pd.personId + "\t" +pd.coord.getX() + "\t" + pd.coord.getY() + "\t" + pd.linkId);
 			writer.newLine();
 		}
 		writer.close();
